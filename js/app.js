f23d041ea508e90847cf775fc24b919f7b661468
den <dkhad@ukr.net>
2021-12-14 22:22:18 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,74 @@
+const container = document.querySelector('.container');
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker
+            .register('/serviceWorker.js', { scope: '/' })
+            .then(res => console.log('service worker registered'))
+            .catch(err => console.log('service worker not registered', err));
+    });
+}
+
+jQuery(function($) {
+    $('#reset').click(function() {
+        const $input = $('input');
+
+        $input.val('');
+        $input.removeClass('is-danger');
+
+        $('#result').text('Результат');
+    });
+
+    $('#calculate').click(function() {
+        $('input').removeClass('is-danger');
+
+        const $result = $('#result');
+        $result.text('Результат');
+
+        if (validate()) {
+            const dose = parseFloat($('#dose').val());
+            const weight = parseFloat($('#weight').val());
+            const capacity = parseFloat($('#capacity').val());
+            const speed = parseFloat($('#speed').val());
+
+            const result = (dose * weight * capacity) / (16.64 * speed);
+            $result.text(result.toFixed(2));
+        }
+    });
+
+    function validate() {
+        const $dose = $('#dose');
+        const $weight = $('#weight');
+        const $capacity = $('#capacity');
+        const $speed = $('#speed');
+
+        const doseVal = parseFloat($dose.val());
+        const weightVal = parseFloat($weight.val());
+        const capacityVal = parseFloat($capacity.val());
+        const speedVal = parseFloat($speed.val());
+
+        let validated = true;
+
+        if (isNaN(doseVal)) {
+            $dose.addClass('is-danger');
+            validated = false;
+        }
+
+        if (isNaN(weightVal)) {
+            $weight.addClass('is-danger');
+            validated = false;
+        }
+
+        if (isNaN(capacityVal)) {
+            $capacity.addClass('is-danger');
+            validated = false;
+        }
+
+        if (isNaN(speedVal)) {
+            $speed.addClass('is-danger');
+            validated = false;
+        }
+
+        return validated;
+    }
+});
---
+++ serviceWorker.js
@@ -0,0 +1,46 @@
+const staticTerraVetSoft = 'static-terravet-soft';
+
+const assets = [
+    '/',
+    '/index.html',
+    '/css/bulma.min.css',
+    '/css/font-awesome.min.css',
+    '/js/jquery-3.6.0.min.js',
+    '/js/app.js',
+    '/images/icons/icon-57.png',
+    '/images/icons/icon-60.png',
+    '/images/icons/icon-72.png',
+    '/images/icons/icon-76.png',
+    '/images/icons/icon-96.png',
+    '/images/icons/icon-114.png',
+    '/images/icons/icon-120.png',
+    '/images/icons/icon-128.png',
+    '/images/icons/icon-144.png',
+    '/images/icons/icon-152.png',
+    '/images/icons/icon-180.png',
+    '/images/icons/icon-192.png',
+    '/images/icons/icon-384.png',
+    '/images/icons/icon-512.png',
+    '/fonts/FontAwesome.otf',
+    '/fonts/fontawesome-webfont.eot',
+    '/fonts/fontawesome-webfont.svg',
+    '/fonts/fontawesome-webfont.ttf',
+    '/fonts/fontawesome-webfont.woff',
+    '/fonts/fontawesome-webfont.woff2',
+];
+
+self.addEventListener('install', installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticTerraVetSoft).then(cache => {
+            cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener('fetch', fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
+});
