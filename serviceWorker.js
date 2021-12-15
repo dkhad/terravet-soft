b3070062f7812685e9e6d2b09d54de6800fa78d8
den <dkhad@ukr.net>
2021-12-15 17:14:34 +0200

    fix service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,41 +1,33 @@
 const staticTerraVetSoft = 'static-terravet-soft-v1';
+const basePath = '/terravet-soft';
 
 const assets = [
-    '/',
-    '/index.html',
-    '/css/bulma.min.css',
-    '/js/jquery-3.6.0.min.js',
-    '/js/app.js',
-    '/images/icons/icon-57.png',
-    '/images/icons/icon-60.png',
-    '/images/icons/icon-72.png',
-    '/images/icons/icon-76.png',
-    '/images/icons/icon-96.png',
-    '/images/icons/icon-114.png',
-    '/images/icons/icon-120.png',
-    '/images/icons/icon-128.png',
-    '/images/icons/icon-144.png',
-    '/images/icons/icon-152.png',
-    '/images/icons/icon-180.png',
-    '/images/icons/icon-192.png',
-    '/images/icons/icon-384.png',
-    '/images/icons/icon-512.png',
+    basePath,
+    `${basePath}/index.html`,
+    `${basePath}/css/bulma.min.css`,
+    `${basePath}/js/jquery-3.6.0.min.js`,
+    `${basePath}/js/app.js`,
+    `${basePath}/images/icons/icon-57.png`,
+    `${basePath}/images/icons/icon-60.png`,
+    `${basePath}/images/icons/icon-72.png`,
+    `${basePath}/images/icons/icon-76.png`,
+    `${basePath}/images/icons/icon-96.png`,
+    `${basePath}/images/icons/icon-114.png`,
+    `${basePath}/images/icons/icon-120.png`,
+    `${basePath}/images/icons/icon-128.png`,
+    `${basePath}/images/icons/icon-144.png`,
+    `${basePath}/images/icons/icon-152.png`,
+    `${basePath}/images/icons/icon-180.png`,
+    `${basePath}/images/icons/icon-192.png`,
+    `${basePath}/images/icons/icon-384.png`,
+    `${basePath}/images/icons/icon-512.png`,
 ];
 
 self.addEventListener('install', installEvent => {
     installEvent.waitUntil(
-        /*
         caches
             .open(staticTerraVetSoft)
             .then(cache => cache.addAll(assets))
-       */
-
-        caches.open(staticTerraVetSoft).then(cache => {
-            cache.add('/terravet-soft' + assets[0]);
-            cache.add('/terravet-soft' + assets[1]);
-            cache.add(assets[2]);
-            cache.add(assets[3]);
-        })
     );
 });
 
